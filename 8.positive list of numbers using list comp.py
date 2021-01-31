dbf2481690eb9b10a40f51c6fef0768be84b1650
THEERTHA T <74696229+THEERTHA-T@users.noreply.github.com>
2021-01-31 12:13:06 +0530

    Update 8.positive list of numbers using list comp.py

--- 8.positive list of numbers using list comp.py
+++ 8.positive list of numbers using list comp.py
@@ -3,6 +3,6 @@
 num=input("Enter list of numbers :")
 list1=[]
 list1=[int(i) for i in num.split()]
-list2=[i for i in list1 if i>0]
+list2=[int(i) for i in list1 if i>0]
 print("ACTUAL LIST =",list1)
 print("POSITIVE NUMBERS IN LIST =",list2)
